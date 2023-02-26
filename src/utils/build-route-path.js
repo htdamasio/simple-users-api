e49c806ecca2c31e58c0edd07ecf84d24ed9cc8f
htdamasio <henriquetome98@gmail.com>
2023-02-26 11:15:16 -0300

    Fix on params regex

--- src/utils/build-route-path.js
+++ src/utils/build-route-path.js
@@ -2,7 +2,7 @@
 // /users/:id
 export function buildRoutePath(path) {
   const routeParametersRegex = /:([a-zA-Z]+)/g
-  const pathWithParams = path.replaceAll(routeParametersRegex, '(?<$1>[a-zA-z0-9\-_]+)')
+  const pathWithParams = path.replaceAll(routeParametersRegex, '(?<$1>[a-zA-Z0-9\-_]+)')
 
   const pathRegex = new RegExp(`^${pathWithParams}(?<query>\\?(.*))?$`)
 
